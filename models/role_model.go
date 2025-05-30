a84bc5d23d4bd65054abc5da343c3b3ebe1b0b93
shehbaazsk <shehbaazwebdev@gmail.com>
2025-05-30 19:44:46 +0530

    restart project

--- models/role_model.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Role struct {
-	BaseModel
-	Name  string `gorm:"unique;not null" json:"name"`
-	Users []User `gorm:"foreignKey:RoleID" json:"users"`
-}
--- models/userprofile_model.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type UserProfile struct {
-	ID        uint   `gorm:"primaryKey" json:"id"`
-	UserID    uint   `gorm:"uniqueIndex" json:"user_id"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Phone     string `json:"phone"`
-}
